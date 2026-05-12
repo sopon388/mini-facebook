1bba2d7382d6c49e368d0019db0825caaa41bba3
sopon <soponchandradevsharma388@gmail.com>
2026-05-12 20:25:11 +0530

    fixed mongoose options

--- backend/server.js
+++ backend/server.js
@@ -24,12 +24,7 @@ app.get('/', (req, res) => {
 
 
 // 🔥 MONGODB ATLAS CONNECT
-mongoose.connect(process.env.MONGO_URI, {
-
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
 
   console.log('MongoDB Connected');
